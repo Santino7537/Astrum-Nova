25c81d5eee5a29d348ac0c1c4a27002a976c55ca
Santino7537 <martinez.santinonicolaset36@gmail.com>
2026-08-31 01:38:02 -0300

    Crea un menú principal lateral básico

---
+++ Scripts/UI/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_create_sim_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_open_sim_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
